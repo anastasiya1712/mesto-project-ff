fa492dccbebe0f36609e7124a785c9c65d8910b9
AnastasiyaPerepechkina <perepechkina.anastasiya.2002@yandex.ru>
2025-01-08 11:45:53 -0800

    08.01

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,14 +1,28 @@
 //Функция для открытия модального окна
-export function openModal(modal) { 
-    modal.classList.add('popup_is-opened');
-    console.log(modal);
-    const closeButton = modal.querySelector('.popup__close');
-    closeButton.addEventListener('click', closeModal);
-  }
+export function openModal(modal) {
+  modal.classList.add('popup_is-opened');
+  const closeButton = modal.querySelector('.popup__close');
+  closeButton.addEventListener('click', (evt) => {
+    console.log(evt.target);
+    closeModal(modal);
+  });
 
-  //Функция для закрытия модального окна
-export function closeModal (event) {
-    const modal = event.target.closest('.popup');
-    modal.classList.remove('popup_is-opened');
+  document.addEventListener('keydown', (evt) => {
+    if (evt.key === 'Escape') {
+      closeModal(modal);
+    }
+  });
+}
 
-  }
+//Функция для закрытия модального окна
+export function closeModal() {
+  const allPopups = document.getElementsByClassName('popup'); 
+  console.log(allPopups);
+
+  allPopups.forEach((element) => {
+    const isActive = element.classList.contains('popup_is-opened');
+    if (isActive) {
+      modal.classList.remove('popup_is-opened');
+    }
+  });
+}
